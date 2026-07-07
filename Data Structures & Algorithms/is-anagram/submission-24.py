41d82382bf462657569eed3a14b309f73b3a7f56
fforson2 <franklynforson64@gmail.com>
2026-07-07 13:27:11 -0700

    Add: is-anagram - submission-24

---
+++ Data Structures & Algorithms/is-anagram/submission-24.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+
+        countMap = {}
+
+        for char in s:
+            countMap[char] = 1 + countMap.get(char, 0)
+
+
+        for char in t:
+            if char not in countMap:
+                return False
+
+            countMap[char] -= 1
+
+        for val in countMap.values():
+            if val < 0:
+                return False
+
+        return True
