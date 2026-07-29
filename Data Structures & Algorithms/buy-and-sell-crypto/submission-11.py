794ccbac4fd539d6f7ce779c9839eeae8c608aee
fforson2 <franklynforson64@gmail.com>
2026-07-29 02:52:41 -0700

    Add: buy-and-sell-crypto - submission-11

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-11.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        maxProfit = 0
+        l , r = 0, 0
+
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                profit = prices[r] - prices[l]
+                maxProfit = max(profit, maxProfit)
+                r += 1
+
+            else:
+                l = r
+                r += 1
+
+        return maxProfit               
+
